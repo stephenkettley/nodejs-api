26bef433ff902c5bcff1bbe62f3932cd9f9602b3
Stephen Kettley <kettleystephen@gmail.com>
2023-06-13 15:37:09 +0200

    update routes

--- src/index.ts
+++ src/index.ts
@@ -9,31 +9,37 @@ app.use(express.urlencoded({ extended: true }));
 // get all posts
 app.get("/posts", async (req, res, next) => {
   console.log(req.body);
+  console.log(req.params);
 });
 
 // create a post
 app.post("/posts", async (req, res, next) => {
   console.log(req.body);
+  console.log(req.params);
 });
 
 // get a post by id
 app.get("/posts/:id", async (req, res, next) => {
   console.log(req.body);
+  console.log(req.params);
 });
 
 // update all posts
 app.put("/posts/:id", async (req, res, next) => {
   console.log(req.body);
+  console.log(req.params);
 });
 
 // delete all posts
 app.delete("/posts/:id", async (req, res, next) => {
   console.log(req.body);
+  console.log(req.params);
 });
 
 // get a post that belong to a user
 app.get("/users/:id/posts", async (req, res, next) => {
   console.log(req.body);
+  console.log(req.params);
 });
 
 app.listen(3000, () => {
