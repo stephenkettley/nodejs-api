453436265351263fd9cba386fbfebd75ec404623
Stephen Kettley <kettleystephen@gmail.com>
2023-06-14 12:30:23 +0200

    changes to routes

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,111 @@
+import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
 import express from "express";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // get all posts
 app.get("/posts", async (req, res, next) => {
-  console.log(req.body);
-  console.log(req.params);
+  try {
+    const posts = await prisma.post.findMany({
+      where: {
+        published: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    res.json({ posts });
+  } catch (error: any) {
+    next(error.message);
+  }
 });
 
 // create a post
 app.post("/posts", async (req, res, next) => {
-  console.log(req.body);
-  console.log(req.params);
+  try {
+    const post = await prisma.post.create({
+      data: {
+        authorId: 1,
+        ...req.body,
+      },
+    });
+    res.json({ post });
+  } catch (error: any) {
+    next(error.message);
+  }
 });
 
 // get a post by id
 app.get("/posts/:id", async (req, res, next) => {
-  console.log(req.body);
-  console.log(req.params);
+  try {
+    const post = await prisma.post.findUnique({
+      where: {
+        id: Number(req.params.id)
+      }
+    })
+    res.json{post}
+  } catch (error: any) {
+    next(error.message);
+  }
 });
 
-// update all posts
+// update a post
 app.put("/posts/:id", async (req, res, next) => {
-  console.log(req.body);
-  console.log(req.params);
+  try {
+    const post = await prisma.post.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: req.body
+      
+    })
+    res.json({post})
+  } catch (error: any) {
+    next(error.message);
+  }
 });
 
-// delete all posts
+// delete a post
 app.delete("/posts/:id", async (req, res, next) => {
-  console.log(req.body);
-  console.log(req.params);
+  try {
+    await prisma.post.delete({
+      where: {
+        id: Number(req.params.id)
+      }
+    })
+  } catch (error: any) {
+    next(error.message);
+  }
 });
+
 
 // get a post that belong to a user
 app.get("/users/:id/posts", async (req, res, next) => {
-  console.log(req.body);
-  console.log(req.params);
+  try {
+    const usersWithPosts = await prisma.user.findUnique({
+      where: {
+        id: Number(req.params.id)
+      },
+      include: {
+        posts: {
+          where: {
+            published: true
+          }
+        }
+      }
+    })
+    const posts = usersWithPosts?.posts
+    res.json({posts})
+  } catch (error: any) {
+    next(error.message);
+  }
 });
+
 
 app.listen(3000, () => {
   console.log("App is listening on port 3000...");
